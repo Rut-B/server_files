c8a3329a8a5b59de908dbe838a50e2d2435e2f32
Rut Biton <32880198+Rut-B@users.noreply.github.com>
2017-12-08 12:19:14 +0200

    the uplwad files to server is work

--- first_up_mm/server.js
+++ first_up_mm/server.js
@@ -1,56 +1,33 @@
-/*var http = require('http');
-var url = require('url');
-var fs = require('fs');
-
-http.createServer(function (req, res) {
-	
-	
-	
-fs.readFile('index.html', function(err, data) {
- res.writeHead(200, {'Content-Type': 'text/html'});
- res.write(data);
- res.end();
-});
-
- var q = url.parse(req.url, true);
- var url_path = q.pathname;
- 
- if(url_path=="/sendEmail"){
-	 var qdata = q.query;
-	 var name =qdata.Name;
-	 var id =qdata.Id;
-	 fs.writeFile('student.txt', name+ ","+id,function(err){
- console.log("written file");
-}); 
-
- }
-
-}).listen(8080);
-*/
-
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var formidable = require('formidable');
 
 http.createServer(function (req, res) {
-	
-	
-	
+
 fs.readFile('index.html', function(err, data) {
  res.writeHead(200, {'Content-Type': 'text/html'});
  res.write(data);
  res.end();
 });
 
- var q = url.parse(req.url, true);
- var url_path = q.pathname;
+
+if (req.url == '/fileupload') {
+	var form = new formidable.IncomingForm();
+	form.parse(req, function (err, fields, files) {
+	var oldpath = files.filetoupload.path;
+	var newpath = files.filetoupload.name;
+	fs.rename(oldpath, newpath, function (err) {
+	if (err) throw err;
+		
+	
+});
+		console.log("oplwod files.");
+	});
+}
  
- if(url_path=="/sendEmail"){
+
 
- console.log("written file");
-
- }
 
 }).listen(8080);
 
- 
